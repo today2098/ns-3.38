863e8634b06fa5de8b46a7551d33beaf1674a6de
Today <ipp.15122098@gmail.com>
2023-05-28 13:58:00 +0900

    [clean] Refactoring

--- scratch/230518_BoidsRelay/plot-total_rx.py
+++ scratch/230518_BoidsRelay/plot-total_rx.py
@@ -8,16 +8,11 @@ OUTPUT_DIR = 'output/boids_relay/'
 PREFIX = 'total_rx-' + sys.argv[1] + '-' + sys.argv[2] + '-' + sys.argv[3] + '-' + sys.argv[4] + '-' + sys.argv[5]
 
 
+df = pd.read_csv(OUTPUT_DIR + 'boids_relay-total_rx.csv')
+
 fig = plt.figure(figsize=(6.4, 4.8))
 ax = fig.add_subplot(111, xlabel='Time [sec]', ylabel='Total Rx [bytes]')
-
-for v in np.arange(2, 7):
-    # print(v)
-    df = pd.read_csv(OUTPUT_DIR + 'boids_relay-total_rx.csv')
-    time = df.time
-    rx = df.total_rx
-    ax.plot(time, rx)
-
+ax.plot(df.time, df.total_rx)
 ax.set_xlim(0.0, 100.0)
 ax.set_ylim(0, 4000000)
 ax.set_title('Total Rx ($W_S: ' + sys.argv[1] + ', W_A: ' + sys.argv[2] +
